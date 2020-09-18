b8486552f08dbeeed7a7b6eceb07055e0a88f762
18nanma <42802604+18nanma@users.noreply.github.com>
2020-09-18 11:59:16 +0530

    1

--- Advanced DataStructures/reverse_k_node_ll.cpp
+++ Advanced DataStructures/reverse_k_node_ll.cpp
@@ -1,92 +1,77 @@
 #include <bits/stdc++.h> 
 using namespace std; 
-
+  
 struct Node { 
-	int data; 
-	struct Node* next; 
+    int data; 
+    struct Node* next; 
 }; 
+  
+struct Node* reverse (struct Node* head, int k)  
+{  
+    Node* curr = head;  
+    Node* next = NULL;  
+    Node* prev = NULL;  
+    int count = 0;  
 
-struct Node* Reverse(struct Node* head, int k) 
+    while (curr != NULL && count < k)  
+    {  
+        next = curr->next;  
+        curr->next = prev;  
+        prev = curr;  
+        curr = next;  
+        count++;  
+    }  
+
+    if (next != NULL)  
+    head->next = reverse(next, k);  
+  
+    return prev;  
+}  
+
+struct Node* insert(struct Node* head, int val) 
 { 
-	stack<Node*> kstack; 
-	struct Node* current = head; 
-	struct Node* prev = NULL; 
+    struct Node* newn =  
+          (struct Node*)malloc(sizeof(struct Node)); 
+    newn->data = val; 
+    newn->next = NULL; 
+    if(head == NULL){
+        return newn;
+    }
+    struct Node* curr = head;
+    while(curr->next != NULL){
+        curr = curr->next;
+    }
+    curr->next = newn;
+    return head;
 
-	while (current != NULL) { 
-
-		int count = 0; 
-		while (current != NULL && count < k) { 
-			kstack.push(current); 
-			current = current->next; 
-			count++; 
-		} 
-
-		while (kstack.size() > 0) { 
-
-			if (prev == NULL) { 
-				prev = kstack.top(); 
-				head = prev; 
-				kstack.pop(); 
-			} else { 
-				prev->next = kstack.top(); 
-				prev = prev->next; 
-				kstack.pop(); 
-			} 
-		} 
-	} 
-
-	prev->next = NULL; 
-
-	return head; 
 } 
+int main() {
+	int n,k;
+	cout<<"Enter the no. of elements"<<endl;
+	cin>>n;
+	int val;
+	struct Node* head = NULL;
+	cout<<"Enter the elements"<<endl;
+	for(int i = 0; i < n; i++){
+	    cin>>val;
+	   head = insert(head, val);
+	}
+	cout<<"Enter k value"<<endl;
+	cin>>k;
+	head = reverse(head, k);
+	while(head!=NULL){
+	    cout<<head->data<<" ";
+	    head = head->next;
+	}
+	return 0;
+}
 
-void push(struct Node** head_ref, int new_data) 
-{ 
-	struct Node* new_node = 
-		(struct Node*)malloc(sizeof(struct Node)); 
-
-	new_node->data = new_data; 
-
-	(*head_ref) = new_node;
-
- 	if (*head_ref == NULL)   
-    	{   
-        	*head_ref = new_node;   
-        	return;   
-    	}   
-    
-    	while (last->next != NULL)   
-        	last = last->next;   
-    	
-	last->next = new_node; 
-	return;  
-} 
-
-void printList(struct Node* node) 
-{ 
-	while (node != NULL) { 
-		printf("%d ", node->data); 
-		node = node->next; 
-	} 
-} 
-
-int main() 
-{ 
-	struct Node* head = NULL; 
-	int e = 0;
-
-	cout << "Enter the numbers and -1 to stop" << endl;
-	while( e != -1){
-		cin >> e;
-		push(&head, e); 
-	} 
-
-	printf("\nGiven linked list \n"); 
-	printList(head); 
-	head = Reverse(head, 3); 
-
-	printf("\nReversed Linked list \n"); 
-	printList(head); 
-
-	return 0; 
-} 
+/* Output:
+Enter the no. of elements
+7
+Enter the elements
+1 2 3 4 5 6 7
+Enter k value
+3
+3 2 1 6 5 4 7 
+*/
