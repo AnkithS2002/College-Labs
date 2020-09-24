54bc27eeeb3332233186b9a2fa871f3d633fd37d
18nanma <42802604+18nanma@users.noreply.github.com>
2020-09-24 15:19:31 +0530

    lab 1

---
+++ Unix-Lab/calculator.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+echo "enter the two numbers"
+read a
+read b
+add=`echo $a \+ $b|bc`
+echo "Sum of $a and $b = $add"
+sub=`echo $a \- $b|bc`
+echo "Difference of $a and $b = $sub"
+mul=`echo $a \* $b|bc`
+echo "Product of $a and $b = $mul"
+div=`echo $a \/ $b|bc`
+echo "Division of $a and $b = $div"
+
---
+++ Unix-Lab/first.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+# Sample Shell Script
+
+echo "Today's date : `date`"
+echo "My Shell : $SHELL"
+echo "This month's calender : `cal`"
+echo "Today's Date : `date "+%d%m%Y"`"
+
+
