bc7325160c6709892d3cce41cf63ed7a003b6f9c
18nanma <42802604+18nanma@users.noreply.github.com>
2020-10-15 15:37:04 +0530

    corrected the program.

--- Unix-Lab/lab-3/largest.sh
+++ Unix-Lab/lab-3/largest.sh
@@ -8,7 +8,7 @@ read n3
 if [ $n1 -gt $n2 ] && [ $n1 -gt $n3 ]
 	then
 		echo "$n1 is the largest of three numbers"
-elif [ $n2 -gt $n1 ] && [ $n2 -gt $n1 ]
+elif [ $n2 -gt $n1 ] && [ $n2 -gt $n3 ]
 	then 
 		echo "$n2 is the largest of three numbers"
 else
