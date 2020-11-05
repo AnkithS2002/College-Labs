97a3002e55f7ab3a1c45cf8612f7f0fc16f2e18e
18nanma <42802604+18nanma@users.noreply.github.com>
2020-11-05 15:07:42 +0530

    update

--- Unix-Lab/lab-5/letterCheck.sh
+++ Unix-Lab/lab-5/letterCheck.sh
@@ -10,6 +10,11 @@ case $ch in
 	"i") echo "It is a vowel.";;
 	"o") echo "It is a vowel.";;
 	"u") echo "It is a vowel.";;
+	"A") echo "It is a vowel.";;
+	"E") echo "It is a vowel.";;
+	"I") echo "It is a vowel.";;
+	"O") echo "It is a vowel.";;
+	"U") echo "It is a vowel.";;
 	*) echo "It is a consonant."
 esac
 
